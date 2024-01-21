ab120ae4138ad46bd5e5416022607320cbc6d959
Dérick Eduardo <dreduardo2222@gmail.com>
2024-01-20 23:40:22 -0400

    Implementado todos os testes iniciais pedidos na issue 5. close #5

--- mercado/base/tests/test_home.py
+++ mercado/base/tests/test_home.py
@@ -20,3 +20,16 @@ def test_title(resp):
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}"')
+
+# # descomentar quando implementar o favicon definitivo
+# def test_favicon(resp):
+#     assert_contains(resp, 'href="/static/img/usuario_icon"')
+#
+# descomentar quando implementar o link e o botão da página definitivo
+# def test_login_link_e_botao(resp):
+#     assert_contains(resp, 'href="<link da página de cadastro de usuário>"')
+#     assert_contains(resp, 'src="/static/img/usuario_icon_3"')
+#
+# descomentar quando implementar o logo do rodapé definitivo
+# def test_logo_rodape(resp):
+#     assert_contains(resp, 'src="/static/img/usuario_icon"')
