729a45e37b601284ec36a3bec2233f2ac01eefee
Dérick Eduardo <dreduardo2222@gmail.com>
2024-01-25 22:23:57 -0400

    Criado view e template para página-detalhe do produto. close #13

--- mercado/produtos/views.py
+++ mercado/produtos/views.py
@@ -1,3 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def produto(request):
+    return render(request, 'produtos/produto_detalhe.html')
