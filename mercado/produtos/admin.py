97d8dfd7c34a449ebc1afca3e998b55b1e7331fd
Dérick Eduardo <dreduardo2222@gmail.com>
2024-02-02 22:50:50 -0400

    Alteração pequena no register do admin. close #26

--- mercado/produtos/admin.py
+++ mercado/produtos/admin.py
@@ -1,19 +1,23 @@
 from django.contrib.admin import ModelAdmin, register
 
+from mercado.produtos.models import Categoria, Departamento, Produto
 
-@register
+
+@register(Produto)
 class ProdutoAdmin(ModelAdmin):
     list_display = ('nome', 'marca', 'categoria', 'data_criacao')
     list_filter = ('marca', 'categoria')
     prepopulated_fields = {'slug': ('nome',)}
 
 
+@register(Categoria)
 class CategoriaAdmin(ModelAdmin):
     list_display = ('nome', 'departamento', 'data_criacao')
     list_filter = ('departamento',)
     prepopulated_fields = {'slug': ('nome',)}
 
 
+@register(Departamento)
 class DepartamentoAdmin(ModelAdmin):
     list_display = ('nome', 'data_criacao')
     prepopulated_fields = {'slug': ('nome',)}
--- mercado/produtos/models.py
+++ mercado/produtos/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     marca = models.CharField(max_length=64)
     codigo = models.CharField(max_length=20, unique=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    slug = models.SlugField(unique=True)
     imagem = models.ImageField(upload_to='imagens_produtos/')
     descricao = models.TextField(null=True)
     categoria = models.ForeignKey('Categoria', on_delete=models.PROTECT)
