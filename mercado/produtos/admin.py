18b043810b9bdc657943da9fdccb9fbf8119deca
Dérick Eduardo <dreduardo2222@gmail.com>
2024-02-02 21:31:59 -0400

    Criado Admin para os modelos: Produto, Categoria e Departamento. close #26

--- mercado/produtos/admin.py
+++ mercado/produtos/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+
+@register
+class ProdutoAdmin(ModelAdmin):
+    list_display = ('nome', 'marca', 'categoria', 'data_criacao')
+    list_filter = ('marca', 'categoria')
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+class CategoriaAdmin(ModelAdmin):
+    list_display = ('nome', 'departamento', 'data_criacao')
+    list_filter = ('departamento',)
+    prepopulated_fields = {'slug': ('nome',)}
+
+
+class DepartamentoAdmin(ModelAdmin):
+    list_display = ('nome', 'data_criacao')
+    prepopulated_fields = {'slug': ('nome',)}
