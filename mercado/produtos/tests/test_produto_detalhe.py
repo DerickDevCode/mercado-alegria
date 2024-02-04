3de448204daa7da842f429ccaacceb6ba0a6aa55
Dérick Eduardo <dreduardo2222@gmail.com>
2024-02-04 16:03:30 -0400

    Adicionado teste status_code.

--- mercado/produtos/tests/test_produto_detalhe.py
+++ mercado/produtos/tests/test_produto_detalhe.py
@@ -1,32 +1,52 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
+
+from mercado.produtos.models import Departamento, Categoria, Produto
 
 
 @pytest.fixture
-def resp(client, db):
-    resp = client.get(reverse('produtos:produto'))
+def departamento(db):
+    return baker.make(Departamento)
+
+
+@pytest.fixture
+def categoria(departamento, db):
+    return baker.make(Categoria, departamento=departamento)
+
+
+@pytest.fixture
+def produto(categoria, db):
+    return baker.make(Produto, categoria=categoria)
+
+
+@pytest.fixture
+def resp(client, departamento, categoria, produto):
+    resp = client.get(reverse('produtos:produto',
+                              kwargs={'departamento': departamento.slug, 'categoria': categoria.slug,
+                                      'slug': produto.slug}))
     return resp
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
-
-def test_nome_produto(resp):
-    pass
-
-
-def test_marca_produto(resp):
-    pass
-
-
-def test_codigo_produto(resp):
-    pass
-
-
-def test_preco_produto(resp):
-    pass
-
-
-def test_descricao_produto(resp):
-    pass
+#
+# def test_nome_produto(resp):
+#     pass
+#
+#
+# def test_marca_produto(resp):
+#     pass
+#
+#
+# def test_codigo_produto(resp):
+#     pass
+#
+#
+# def test_preco_produto(resp):
+#     pass
+#
+#
+# def test_descricao_produto(resp):
+#     pass
