843c1361162d6fe2ca60b97205a91e69ed2ef098
Dérick Eduardo <dreduardo2222@gmail.com>
2024-02-11 16:20:15 -0400

    Implementada página de departamento dos Produtos. close #30 close #33 close #34

--- mercado/produtos/views.py
+++ mercado/produtos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils.text import slugify
 
 from mercado.produtos import facade
 
@@ -7,14 +8,17 @@ def produto(request, departamento, categoria, slug):
     departamento = facade.buscar_departamento_da_categoria(slug)
     categoria = facade.buscar_categoria_do_produto(slug)
     produto = facade.buscar_produto(slug)
+    marca = slugify(produto.marca)
     return render(request, 'produtos/produto_detalhe.html',
-                  context={'departamento': departamento, 'categoria': categoria, 'produto': produto})
+                  context={'departamento': departamento, 'categoria': categoria, 'produto': produto, 'marca': marca})
 
 
 def pagina_de_marcas(request, marca):
     produtos = facade.listar_produtos_por_marca(marca)
-    marca = produtos[0].marca
-    return render(request, 'produtos/produtos_por_marca.html', context={'produtos': produtos, 'marca': marca})
+    marca_slug = slugify(marca)
+    # marca = produtos[0].marca
+    return render(request, 'produtos/produtos_por_marca.html',
+                  context={'produtos': produtos, 'marca': marca, 'marca_slug': marca_slug})
 
 
 def pagina_de_departamentos(request, departamento):
