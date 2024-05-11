654d75e47b4aa4173f6e86d5bd5d9f99924b4d0a
Dérick Eduardo <dreduardo2222@gmail.com>
2024-05-10 23:04:57 -0400

    Resolvido o problema da senha inválida ao cadastrar o usuário.

--- mercado/base/views.py
+++ mercado/base/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
 
 from mercado.base.forms import UserForm
+from mercado.base.models import User
 
 
 def home(request):
@@ -15,6 +17,9 @@ def cadastro(request):
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = User.objects.get(email=form.cleaned_data['email'])
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
             return render(request, 'base/home.html')
         else:
             return render(request, 'base/cadastro.html', context={'form': form})
