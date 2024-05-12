62ad56d958b176a0d658f9ebbf4d326917abd11d
Dérick Eduardo <dreduardo2222@gmail.com>
2024-05-12 19:21:46 -0400

    Criada fixture global client com usuario logado.

---
+++ mercado/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from mercado.base.models import User
+
+
+@pytest.fixture
+def usuario_logado(db):
+    usuario_modelo = User.objects.create_user(first_name='teste', last_name='testando', date_of_birth='2000-01-01',
+                                              email='usuario@exemplo.com',
+                                              cpf='000.000.000-00', phone_number='00 00000-0000', password='teste')
+    return usuario_modelo
+
+
+@pytest.fixture
+def client_com_usuario_logado(usuario_logado, client):
+    client.force_login(usuario_logado)
+    return client
