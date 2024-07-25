f4ef45e1c1d7493cfe28e1905a8e323d3082c073
Dérick Eduardo <dreduardo2222@gmail.com>
2024-07-24 22:45:36 -0400

    Criado página de exibição para 'Políticas de Cancelamentos e Ressarcimentos'.

--- mercado/base/urls.py
+++ mercado/base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from mercado.base.views import home, cadastro, perfil, quem_somos_nos, politica_de_privacidade, politica_de_entrega, \
-    trocas_e_devolucoes
+    trocas_e_devolucoes, políticas_de_cancelamentos_e_ressarcimentos
 
 app_name = 'base'
 urlpatterns = [
@@ -12,4 +12,6 @@ urlpatterns = [
     path('politica_de_privacidade', politica_de_privacidade, name='politica_de_privacidade'),
     path('politica_de_entrega', politica_de_entrega, name='politica_de_entrega'),
     path('trocas_e_devolucoes', trocas_e_devolucoes, name='trocas_e_devolucoes'),
+    path('políticas_de_cancelamentos_e_ressarcimentos', políticas_de_cancelamentos_e_ressarcimentos,
+         name='políticas_de_cancelamentos_e_ressarcimentos'),
 ]
