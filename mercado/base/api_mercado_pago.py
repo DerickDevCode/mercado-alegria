64601e8d3d3cb7b8997006a68c230eb71e5aa28b
Dérick Eduardo <dreduardo2222@gmail.com>
2024-09-24 12:33:35 -0400

    Feita integração para que o botão de 'finalizar compra' do carrinho redirecione para a página de pagamentos do mercado pago.

--- mercado/base/api_mercado_pago.py
+++ mercado/base/api_mercado_pago.py
@@ -3,33 +3,36 @@ import mercadopago
 
 from mercado.settings import TOKEN_MERCADO_PAGO
 
-# Adicione as credenciais
-sdk = mercadopago.SDK(TOKEN_MERCADO_PAGO)
 
-# Cria um item na preferência
-preference_data = {
-    "items": [
-        {
-            "id": "1",
-            "title": "Produto teste",
-            # "description": "Dummy description",
-            # "picture_url": "http://www.myapp.com/myimage.jpg",
-            # "category_id": "car_electronics",
-            "quantity": 1,
-            "currency_id": "BRL",
-            "unit_price": 1,
-        }
-    ],
-    "back_urls": {
-        "success": "http://test.com/success",
-        "failure": "http://test.com/failure",
-        "pending": "http://test.com/pending",
-    },
-    "auto_return": "all"
-}
+def gerar_link_de_pagamento():
+    # Adicione as credenciais
+    sdk = mercadopago.SDK(TOKEN_MERCADO_PAGO)
 
-preference_response = sdk.preference().create(preference_data)
-preference = preference_response["response"]
+    # Cria um item na preferência
+    preference_data = {
+        "items": [
+            {
+                "id": "1",
+                "title": "Produto teste",
+                # "description": "Dummy description",
+                # "picture_url": "http://www.myapp.com/myimage.jpg",
+                # "category_id": "car_electronics",
+                "quantity": 1,
+                "currency_id": "BRL",
+                "unit_price": 1,
+            }
+        ],
+        "back_urls": {
+            "success": "http://test.com/success",
+            "failure": "http://test.com/failure",
+            "pending": "http://test.com/pending",
+        },
+        "auto_return": "all"
+    }
 
-print(preference)
-print(preference['init_point'])
+    preference_response = sdk.preference().create(preference_data)
+    preference = preference_response["response"]
+    return preference['init_point']
+
+# print(preference)
+# print(preference['init_point'])
