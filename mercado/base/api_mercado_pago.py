5c42257b514c0657ab54459e72fb6305dbb3287e
Dérick Eduardo <dreduardo2222@gmail.com>
2024-09-23 22:33:24 -0400

    Criado arquivo de configuração da API de pagamentos do mercado pago.

---
+++ mercado/base/api_mercado_pago.py
@@ -0,0 +1,35 @@
+# SDK do Mercado Pago
+import mercadopago
+
+from mercado.settings import TOKEN_MERCADO_PAGO
+
+# Adicione as credenciais
+sdk = mercadopago.SDK(TOKEN_MERCADO_PAGO)
+
+# Cria um item na preferência
+preference_data = {
+    "items": [
+        {
+            "id": "1",
+            "title": "Produto teste",
+            # "description": "Dummy description",
+            # "picture_url": "http://www.myapp.com/myimage.jpg",
+            # "category_id": "car_electronics",
+            "quantity": 1,
+            "currency_id": "BRL",
+            "unit_price": 1,
+        }
+    ],
+    "back_urls": {
+        "success": "http://test.com/success",
+        "failure": "http://test.com/failure",
+        "pending": "http://test.com/pending",
+    },
+    "auto_return": "all"
+}
+
+preference_response = sdk.preference().create(preference_data)
+preference = preference_response["response"]
+
+print(preference)
+print(preference['init_point'])
