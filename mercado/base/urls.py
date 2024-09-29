f8c2087355e91bff2e1a4fbf05cc0df3d8f3dd0f
Dérick Eduardo <dreduardo2222@gmail.com>
2024-09-28 23:34:09 -0400

    Criado páginas de falha e sucesso do pagamento.

--- mercado/base/urls.py
+++ mercado/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from mercado.base.views import home, cadastro, perfil, editar_perfil, quem_somos_nos, politica_de_privacidade, \
     politica_de_entrega, trocas_e_devolucoes, politicas_de_cancelamentos_e_ressarcimentos, \
-    politica_de_venda_precos_ofertas_e_promocoes, central_de_atendimento
+    politica_de_venda_precos_ofertas_e_promocoes, central_de_atendimento, pagamento_sucesso, pagamento_falha
 
 app_name = 'base'
 urlpatterns = [
@@ -19,4 +19,6 @@ urlpatterns = [
     path('politica_de_venda_precos_ofertas_e_promocoes', politica_de_venda_precos_ofertas_e_promocoes,
          name='politica_de_venda_precos_ofertas_e_promocoes'),
     path('central_de_atendimento', central_de_atendimento, name='central_de_atendimento'),
+    path('pagamento_sucesso', pagamento_sucesso, name='pagamento_sucesso'),
+    path('pagamento_falha', pagamento_falha, name='pagamento_falha'),
 ]
