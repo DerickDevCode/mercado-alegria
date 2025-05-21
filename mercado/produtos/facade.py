7de6c2d792e3c1c8b36292f53c105a076a6243b4
Dérick Eduardo <dreduardo2222@gmail.com>
2025-05-20 21:09:45 -0400

    Exclui 3 funções e altera 4 do facade. Altera a view produto.

--- mercado/produtos/facade.py
+++ mercado/produtos/facade.py
@@ -10,39 +10,12 @@ def buscar_produto(slug):
     return Produto.objects.get(slug=slug)
 
 
-def buscar_subcategoria_do_produto(slug):
-    """
-    Encontra a subcategoria de um produto solicitado
-    :param slug: recebe o slug do produto
-    :return: retorna a subcategoria do produto referente ao slug
-    """
-    return Produto.objects.get(slug=slug).subcategoria.slug
-
-
-def buscar_categoria_do_produto(slug):
-    """
-    Encontra a categoria de um produto solicitado
-    :param slug: recebe o slug do produto
-    :return: retorna a categoria do produto referente ao slug
-    """
-    return Produto.objects.get(slug=slug).subcategoria.categoria.slug
-
-
-def buscar_departamento_do_produto(slug):
-    """
-    Encontra o departamento de um produto solicitado
-    :param slug: recebe o slug do produto
-    :return: retorna o departamento do produto
-    """
-    return Produto.objects.get(slug=slug).subcategoria.categoria.departamento.slug
-
-
 def listar_produtos_por_marca(marca):
     """
     Lista todos os produtos de uma determinada marca
     :return: retorna uma lista com os produtos de uma determinada marca ordenados pelo slug
     """
-    return list(Produto.objects.filter(marca=marca).order_by('slug'))
+    return Produto.objects.filter(marca=marca).order_by('slug')
 
 
 def listar_produtos_por_departamento(departamento):
@@ -50,8 +23,7 @@ def listar_produtos_por_departamento(departamento):
     Lista todos os produtos de um determinado departamento
     :return: retorna uma lista com os produtos de um determinado departamento ordenados pelo slug
     """
-    return list(
-        Produto.objects.filter(subcategoria__categoria__departamento__slug__iexact=departamento).order_by('slug'))
+    return Produto.objects.filter(subcategoria__categoria__departamento__slug=departamento).order_by('slug')
 
 
 def listar_produtos_por_categoria(categoria):
@@ -59,7 +31,7 @@ def listar_produtos_por_categoria(categoria):
     Lista todos os produtos de uma determinada categoria
     :return: retorna uma lista com os produtos de uma determinada categoria ordenados pelo slug
     """
-    return list(Produto.objects.filter(subcategoria__categoria__slug__iexact=categoria).order_by('slug'))
+    return Produto.objects.filter(subcategoria__categoria__slug=categoria).order_by('slug')
 
 
 def listar_produtos_por_subcategoria(subcategoria):
@@ -67,7 +39,7 @@ def listar_produtos_por_subcategoria(subcategoria):
     Lista todos os produtos de uma determinada subcategoria
     :return: retorna uma lista com os produtos de uma determinada subcategoria ordenados pelo slug
     """
-    return list(Produto.objects.filter(subcategoria__slug__iexact=subcategoria).order_by('slug'))
+    return Produto.objects.filter(subcategoria__slug=subcategoria).order_by('slug')
 
 
 def filtrar_produtos_pela_pesquisa(query):
--- mercado/produtos/views.py
+++ mercado/produtos/views.py
@@ -9,13 +9,12 @@ from mercado.produtos.models import Produto, ItemFavoritos
 
 
 def produto(request, departamento, categoria, subcategoria, slug):
-    departamento = facade.buscar_departamento_do_produto(slug)
-    categoria = facade.buscar_categoria_do_produto(slug)
-    subcategoria = facade.buscar_subcategoria_do_produto(slug)
     produto = facade.buscar_produto(slug)
     marca = produto.marca
     return render(request, 'produtos/produto_detalhe.html',
-                  context={'departamento': departamento, 'categoria': categoria, 'subcategoria': subcategoria,
+                  context={'departamento': produto.subcategoria.categoria.departamento.slug,
+                           'categoria': produto.subcategoria.categoria.slug,
+                           'subcategoria': produto.subcategoria.slug,
                            'produto': produto, 'marca': marca})
 
 
@@ -26,18 +25,18 @@ def pagina_de_departamentos(request, departamento):
                   context={'produtos': produtos, 'departamento': departamento})
 
 
-def pagina_de_subcategorias(request, departamento, categoria, subcategoria):
-    produtos = facade.listar_produtos_por_subcategoria(subcategoria)
-    subcategoria = produtos[0].subcategoria.nome
-    return render(request, 'produtos/produtos_por_subcategoria.html',
-                  context={'produtos': produtos, 'subcategoria': subcategoria})
-
-
 def pagina_de_categorias(request, departamento, categoria):
     produtos = facade.listar_produtos_por_categoria(categoria)
     categoria = produtos[0].subcategoria.categoria.nome
     return render(request, 'produtos/produtos_por_categoria.html',
                   context={'produtos': produtos, 'categoria': categoria})
+
+
+def pagina_de_subcategorias(request, departamento, categoria, subcategoria):
+    produtos = facade.listar_produtos_por_subcategoria(subcategoria)
+    subcategoria = produtos[0].subcategoria.nome
+    return render(request, 'produtos/produtos_por_subcategoria.html',
+                  context={'produtos': produtos, 'subcategoria': subcategoria})
 
 
 def pagina_de_marcas(request, marca):
